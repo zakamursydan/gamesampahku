69256f4f9facc1a4a91b15a1ed32812a4f32baa3
zaka <zakamursydan@gmail.com>
2018-02-16 03:45:08 +0700

    GameSampahku
    TUGAS1, ZAKA KHARIS M, NOVIA SINDI R, RIFKI ARDITYA

---
+++ GameSampah/Assets/SampahHilang.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampahHilang : MonoBehaviour {
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		switch(other.gameObject.tag)
+		{
+		case "tongSampah":
+			Destroy (this.gameObject);
+			Debug.Log ("Masuk");
+			break;
+		case "Plan":
+			Destroy (this.gameObject);
+			Debug.Log ("Tidak Masuk");
+			break;
+		}
+}
+}
